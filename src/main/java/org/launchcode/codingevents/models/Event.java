c4682d3f365c285e30fa368d54a6b5f0d2b8284a
revathi <arevathi125@gmail.com>
2024-05-03 14:16:57 -0500

    Add property commit

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,17 +3,27 @@ package org.launchcode.codingevents.models;
 public class Event {
 
     private String name;
+    private String description;
 
-    public Event(String name) {
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
