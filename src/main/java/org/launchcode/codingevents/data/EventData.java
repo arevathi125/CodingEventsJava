866cecfe96b33050febc4e08881f872a023bf833
revathi <arevathi125@gmail.com>
2024-05-03 14:55:11 -0500

    create data layer committed

---
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EventData {
+
+    // Need to place to put events
+     private static Map<Integer, Event> events = new HashMap<>();
+
+    // Get all events
+    public static Collection<Event> getAll(){
+         return events.values();
+    }
+
+    // Get a single event
+     public static Event getById(int id){
+         return events.get(id);
+     }
+
+    // Add an event
+     public static void add(Event event){
+          events.put(event.getId(), event);
+     }
+
+    // Delete an event
+     public static void remove(int id){
+         events.remove(id);
+     }
+}
